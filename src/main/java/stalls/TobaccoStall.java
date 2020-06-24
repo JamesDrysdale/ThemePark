f95e47c68e9071af402002a2c27542fbbf283d00
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-06-24 07:03:35 +0100

    Tobacco stall can be accessed by persons aged 18 or over. Persons aged less than 18 cannot access

--- src/main/java/stalls/TobaccoStall.java
+++ src/main/java/stalls/TobaccoStall.java
@@ -1,12 +1,19 @@
 package stalls;
 
+import behaviours.ISecurity;
 import people.Visitor;
 
-public class TobaccoStall extends Stall{
+public class TobaccoStall extends Stall implements ISecurity {
 
     public TobaccoStall(String name, String ownerName, ParkingSpot parkingSpot) {
         super(name, ownerName, parkingSpot);
     }
 
-
+    // Stall has min age limit of 18
+    public boolean isAllowed(Visitor visitor){
+        if (visitor.getAge() >= 18){
+            return true;
+        }
+        return false;
+    }
 }
