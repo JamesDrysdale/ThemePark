047ae6d7a39cfa185cc4ff2b773c08dbdb3471da
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-06-24 07:01:15 +0100

    Tests for tobacco stall age limit of 18

--- src/main/java/stalls/TobaccoStall.java
+++ src/main/java/stalls/TobaccoStall.java
@@ -1,8 +1,12 @@
 package stalls;
 
-public class TobaccoStall extends Stall {
+import people.Visitor;
+
+public class TobaccoStall extends Stall{
 
     public TobaccoStall(String name, String ownerName, ParkingSpot parkingSpot) {
         super(name, ownerName, parkingSpot);
     }
+
+
 }
--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,23 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor youngVisitor;
+    Visitor youngAdult;
+    Visitor adult;
 
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        youngVisitor = new Visitor(12, 180, 20);
+        youngAdult = new Visitor(18, 250, 50);
+        adult = new Visitor(28, 250, 50);
     }
 
     @Test
@@ -28,4 +35,21 @@ public class TobaccoStallTest {
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
     }
+
+    @Test
+    public void hasMinAge18__youngerVisitorsCannotAccess(){
+        assertFalse(tobaccoStall.isAllowed(youngVisitor));
+    }
+
+    @Test
+    public void hasMinAge18__18YearOldCanAccess(){
+        assertTrue(tobaccoStall.isAllowed(youngAdult));
+    }
+
+    @Test
+    public void hasMinAge18__Over18YearOldCanAccess(){
+        assertTrue(tobaccoStall.isAllowed(adult));
+    }
+
+
 }
