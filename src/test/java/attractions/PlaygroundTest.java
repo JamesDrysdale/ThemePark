755ac3dbba8225afe3b4c81b8ac2f87c502b7f26
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-06-24 06:28:14 +0100

    Add test that children aged 15 or less can ride playground

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,19 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PlaygroundTest {
     Playground playground;
+    Visitor youngVisitor;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        youngVisitor = new Visitor(12, 180, 20);
     }
 
     @Test
@@ -26,5 +30,10 @@ public class PlaygroundTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
+    }
+
+    @Test
+    public void hasMaxAge15__guestsAgedLessThanMaxCanRide(){
+        assertTrue(true, playground.isAllowed(youngVisitor));
     }
 }
